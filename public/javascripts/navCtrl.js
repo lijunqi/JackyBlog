b85cb3315886d6d6de71c05a691973e0ba82a89f
Jacky <lijunqi001@163.com>
2017-09-03 16:44:49 +0800

    add "sign in" page, not done.

--- public/javascripts/navCtrl.js
+++ public/javascripts/navCtrl.js
@@ -3,23 +3,23 @@ var aa = [
 	'blog-nav-item',
 ];
 
-var app = new Vue({
-    el: '#nav',
-    data: {
-	a1: aa[0],
-	a5: aa[1]
-    },
-    methods: {
-      click1: function() {
-	aa = ['blog-nav-item active', 'blog-nav-item'];
-	this.a1 = aa[0];
-	this.a5 = aa[1];
-      },
-      click5: function() {
-	aa = ['blog-nav-item', 'blog-nav-item active'];
-	this.a1 = aa[0];
-	this.a5 = aa[1];
-      }
-    }
-});
+//var app = new Vue({
+//    el: '#nav',
+//    data: {
+//	a1: aa[0],
+//	a5: aa[1]
+//    },
+//    methods: {
+//      click1: function() {
+//	aa = ['blog-nav-item active', 'blog-nav-item'];
+//	this.a1 = aa[0];
+//	this.a5 = aa[1];
+//      },
+//      click5: function() {
+//	aa = ['blog-nav-item', 'blog-nav-item active'];
+//	this.a1 = aa[0];
+//	this.a5 = aa[1];
+//      }
+//    }
+//});
 
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: "Jacky's Blog" });
+  res.render('newHome', { title: "Jacky's Blog" });
 });
 
 router.get('/about', function(req, res, next) {
   res.render('about');
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin');
 });
 
 //var v = require('../public/javascripts/navCtrl');
