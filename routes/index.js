8f1e76c0c0e3a4032621ff26c821ae585607d240
Jacky <lijunqi001@163.com>
2017-09-04 23:13:06 +0800

    modify navigator bar. not done...

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('newHome', { title: "Jacky's Blog" });
+  res.render('home', { title: "Jacky's Blog" });
 });
 
 router.get('/about', function(req, res, next) {
