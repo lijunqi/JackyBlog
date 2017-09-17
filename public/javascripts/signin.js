d95704c131d8c065aa612d6608bb9bda4fce680a
Jacky Li <lijunqi001@163.com>
2017-09-17 22:54:31 +0800

    new sign in js

---
+++ public/javascripts/signin.js
@@ -0,0 +1,14 @@
+
+var signinComp = new Vue({
+	el: '#signin',
+	data: {
+		unameVal: '',
+		passwordVal: ''
+	},
+	methods: {
+		test: function (event) {
+			console.log(`uname: ${this.unameVal}, pwd: ${this.passwordVal}`);
+			console.log("Password: ", this.passwordVal);
+		}
+	}
+});
